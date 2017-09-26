265732bc64c6b11a52da9ca9ebbf5945c73430f0
Parth Vakil <pvakil@praemittias.com>
2017-09-26 17:20:11 -0400

    Updated hist_stoc_data from mathworks website and appropriately chaged retrieveStockHistory to use this file

--- analysis/matlab/hist_stock_data.m
+++ analysis/matlab/hist_stock_data.m
@@ -2,15 +2,27 @@ function stocks = hist_stock_data(start_date, end_date, varargin)
 % HIST_STOCK_DATA     Obtain historical stock data
 %   hist_stock_data(X,Y,'Ticker1','Ticker2',...) retrieves historical stock
 %   data for the ticker symbols Ticker1, Ticker2, etc... between the dates
-%   specified by X and Y.  X and Y are strings in the format ddmmyyyy,
-%   where X is the beginning date and Y is the ending date.  The program
-%   returns the stock data in a structure giving the Date, Open, High, Low,
-%   Close, Volume, and Adjusted Close price adjusted for dividends and
-%   splits.
+%   specified by X and Y.  X and Y can either be strings in the format
+%   ddmmyyyy or Matlab datenums, where X is the beginning date and Y is the
+%   ending date.  The program returns the stock data in a structure giving
+%   the Date, Open, High, Low, Close, Volume, and Adjusted Close price
+%   adjusted for dividends and splits.
 %
 %   hist_stock_data(X,Y,'tickers.txt') retrieves historical stock data
 %   using the ticker symbols found in the user-defined text file.  Ticker
 %   symbols must be separated by line feeds.
+%
+%   hist_stock_data(X,Y,{'Ticker1' 'Ticker2'}) combined the ticker symbols
+%   into a single cell array when calling hist_stock_data (sometimes easier
+%   for calling the funtion with a cell array of ticker symbols).
+%
+%   hist_stock_data(X,Y,'Ticker1','frequency',FREQ) retrieves historical
+%   stock data using the frequency specified by FREQ, which must be either
+%   'd' for daily, 'wk' for weekly, or 'mo' for monthly.
+%
+%   hist_stock_data(X,Y,'Ticker1','type','div') retrieves dividend data. If
+%   anything but 'div' is specified then it will default to retrieving
+%   historical prices.
 %
 %   EXAMPLES
 %       stocks = hist_stock_data('23012003','15042008','GOOG','C');
@@ -25,11 +37,19 @@ function stocks = hist_stock_data(start_date, end_date, varargin)
 %           2001.  The text file must be a column of ticker symbols
 %           separated by new lines.
 %
+%       stocks = hist_stock_data(now-10, now, {'GOOG' 'C'});
+%           Get stock data for approximately the last 10 days for the two
+%           tickers specified in the cell array.
+%
 %       stocks = hist_stock_data('12101997','18092001','C','frequency','w')
 %           Returns historical stock data for Citibank using the date range
 %           specified with a frequency of weeks.  Possible values for
-%           frequency are d (daily), w (weekly), or m (monthly).  If not
+%           frequency are d (daily), wk (weekly), or mo (monthly).  If not
 %           specified, the default frequency is daily.
+%
+%       stocks = hist_stock_data('12101997','18092001','C','type','div')
+%            Returned historical dividend data for Citibank between Oct 12,
+%            1997 and September 18, 2001.
 %
 %   DATA STRUCTURE
 %       INPUT           DATA STRUCTURE      FORMAT
@@ -37,6 +57,8 @@ function stocks = hist_stock_data(start_date, end_date, varargin)
 %       Y (end date)    ddmmyyyy            String
 %       Ticker          NA                  String 
 %       ticker.txt      NA                  Text file
+%       FREQ            NA                  String; 'd', 'wk', or 'mo'
+%       TYPE            NA                  String; 'div'
 %
 %   OUTPUT FORMAT
 %       All data is output in the structure 'stocks'.  Each structure
@@ -63,17 +85,25 @@ function stocks = hist_stock_data(start_date, end_date, varargin)
 
 stocks = struct([]);        % initialize data structure
 
-% split up beginning date into day, month, and year.  The month is
-% subracted is subtracted by 1 since that is the format that Yahoo! uses
-bd = start_date(1:2);       % beginning day
-bm = sprintf('%02d',str2double(start_date(3:4))-1); % beginning month
-by = start_date(5:8);       % beginning year
+%% Parse inputs
 
-% split up ending date into day, month, and year.  The month is subracted
-% by 1 since that is the format that Yahoo! uses
-ed = end_date(1:2);         % ending day
-em = sprintf('%02d',str2double(end_date(3:4))-1);   % ending month
-ey = end_date(5:8);         % ending year
+% Format start and end dates into Posix times. This is the number of
+% seconds since Jan 1, 1970. This previously used the posixtime function,
+% but since that is relatively new, it now does the calculation using
+% Matlab datenum's, which are in units of days, then converting to seconds.
+origDate = datenum('01-Jan-1970 00:00:00', 'dd-mmm-yyyy HH:MM:SS');
+
+% Convert input dates to Matlab datenums, if necessary
+if ischar(start_date)
+    startDate = (datenum(start_date, 'ddmmyyyy') - origDate) * 24 * 60 * 60;
+else
+    startDate = (floor(start_date) - origDate) * 24 * 60 * 60;
+end
+if ischar(end_date)
+    endDate = (datenum(end_date, 'ddmmyyyy') - origDate) * 24 * 60 * 60;
+else
+    endDate = (floor(end_date) - origDate) * 24 * 60 * 60;
+end
 
 % determine if user specified frequency
 temp = find(strcmp(varargin,'frequency') == 1); % search for frequency
@@ -85,14 +115,33 @@ else                                        % if user supplies frequency
 end
 clear temp
 
-% Determine if user supplied ticker symbols or a text file
-if isempty(strfind(varargin{1},'.txt'))     % If individual tickers
-    tickers = varargin;                     % obtain ticker symbols
-else                                        % If text file supplied
+% determine if user specified event type
+temp = find(strcmp(varargin,'type') == 1); % search for frequency
+if isempty(temp)                            % if not given
+    event = 'history';                      % default is historical prices
+else                                        % if user supplies frequency
+    event = varargin{temp+1};               % assign to user input
+    varargin(temp:temp+1) = [];             % remove from varargin
+end
+clear temp
+
+% If the first cell of varargin is itself a cell array, assume it is a cell
+% array of ticker symbols
+if iscell(varargin{1})
+    tickers = varargin{1};
+
+% Otherwise, check to see if it's a .txt file
+elseif ~isempty(strfind(varargin{1},'.txt'))
     fid = fopen(varargin{1}, 'r');
     tickers = textscan(fid, '%s'); tickers = tickers{:};
     fclose(fid);
+    
+% Otherwise, assume it's either a single ticker or a list of tickers
+else
+    tickers = varargin;
 end
+
+%% Get historical data
 
 h = waitbar(0, 'Please Wait...');           % create waitbar
 idx = 1;                                    % idx for current stock data
@@ -101,29 +150,42 @@ idx = 1;                                    % idx for current stock data
 for i = 1:length(tickers)
     
     % Update waitbar to display current ticker
-    waitbar((i-1)/length(tickers),h,sprintf('%s %s %s%0.2f%s', ...
-        'Retrieving stock data for',tickers{i},'(',(i-1)*100/length(tickers),'%)'))
-        
-    % Download historical data using the Yahoo! Finance website
-%     [temp, status] = urlread(strcat('http://ichart.finance.yahoo.com/table.csv?s='...
-%         ,tickers{i},'&a=',bm,'&b=',bd,'&c=',by,'&d=',em,'&e=',ed,'&f=',...
-%         ey,'&g=',freq,'&ignore=.csv'));
-
-[temp, status] = urlread(strcat('http://ichart.finance.yahoo.com/table.csv?s='...
-        ,tickers{i},'&a=',bm,'&b=',bd,'&c=',by,'&d=',em,'&e=',ed,'&f=',ey));
+    waitbar((i-1)/length(tickers), h, ...
+        sprintf('Retrieving stock data for %s (%0.2f%%)', ...
+        tickers{i}, (i-1)*100/length(tickers)))
     
-    % If data was downloaded successfulle, then proceed to process it.
+    % Create url string for retrieving data
+    url = sprintf(['https://query1.finance.yahoo.com/v7/finance/download/', ...
+        '%s?period1=%d&period2=%d&interval=1%s&events=%s'], ...
+        tickers{i}, startDate, endDate, freq, event);
+    
+    % Call data from Yahoo Finance
+    [temp, status] = urlread(url,'post',{'matlabstockdata@yahoo.com', 'historical stocks'});
+        
+    % If data was downloaded successfully, then proceed to process it.
     % Otherwise, ignore this ticker symbol
     if status
         
-        % Parse out the historical data
-        data = textscan(temp, '%s%f%f%f%f%f%f', 'delimiter', ',', ...
-            'Headerlines', 1);
-        
         % Put data into appropriate variables
-        [stocks(idx).Date, stocks(idx).Open, stocks(idx).High, ...
-            stocks(idx).Low, stocks(idx).Close, stocks(idx).Volume, ...
-            stocks(idx).AdjClose] = deal(data{:});
+        if strcmp(event, 'history')     % If historical prices
+            
+            % Parse out the historical data
+            data = textscan(temp, '%s%f%f%f%f%f%f', 'delimiter', ',', ...
+                'Headerlines', 1);
+
+            [stocks(idx).Date, stocks(idx).Open, stocks(idx).High, ...
+                stocks(idx).Low, stocks(idx).Close, ...
+                stocks(idx).AdjClose, stocks(idx).Volume] = deal(data{:});
+            
+        % If dividends
+        else
+            
+            % Parse out the dividend data
+            data = textscan(temp, '%s%f', 'delimiter', ',', ...
+                'Headerlines', 1);
+            
+            [stocks(idx).Date, stocks(idx).Dividend] = deal(data{:});
+        end
 
         stocks(idx).Ticker = tickers{i};	% Store ticker symbol
         
--- analysis/matlab/retrieveStockHistory.m
+++ analysis/matlab/retrieveStockHistory.m
@@ -1,12 +1,12 @@
-function stocks = retrieveStockHistory(start_date, end_date, varargin);
+function [stocks] = retrieveStockHistory(start_date, end_date, varargin);
 
 stocks = hist_stock_data(start_date, end_date, cell2mat(varargin));
 for i = 1:length(stocks)
-    stocks(i).Date = flipud(stocks(i).Date);
-    stocks(i).Open = flipud(stocks(i).Open);
-    stocks(i).High = flipud(stocks(i).High);
-    stocks(i).Low = flipud(stocks(i).Low);
-    stocks(i).Close = flipud(stocks(i).Close);
-    stocks(i).Volume = flipud(stocks(i).Volume);
-    stocks(i).AdjClose = flipud(stocks(i).AdjClose);
+    stocks(i).Date = (stocks(i).Date);
+    stocks(i).Open = (stocks(i).Open);
+    stocks(i).High = (stocks(i).High);
+    stocks(i).Low = (stocks(i).Low);
+    stocks(i).Close = (stocks(i).Close);
+    stocks(i).Volume = (stocks(i).Volume);
+    stocks(i).AdjClose = (stocks(i).AdjClose);
 end
